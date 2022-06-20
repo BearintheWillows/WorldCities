0f49f9cdac537e994f2084cd93d9a2c0c80e18e0
BearintheWillows <sjwillows91@outlook.com>
2022-06-20 20:58:59 +0100

    Added City and Country entities and set properties.

---
+++ WorldCitiesApi/Data/Models/CityModel.cs
@@ -0,0 +1,58 @@
+namespace WorldCitiesApi.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class CityModel
+{
+    #region Properties
+
+    /// <summary>
+    /// The unique id and primary key for this city
+    /// </summary>	
+
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// City name (in UTF8 format)
+    /// </summary>
+
+    public string Name { get; set; }
+
+    /// <summary>
+    /// City Latitude
+    /// </summary>
+
+    [Column( TypeName = "decimal(7,4" )]
+    public decimal Lat { get; set; }
+
+    /// <summary>
+    /// City Longitude
+    /// </summary>
+
+    [Column( TypeName = "decimal(7,4" )]
+    public decimal Lon { get; set; }
+
+    ///<summary>
+    /// Country ID
+    /// </summary
+
+    [ForeignKey( nameof( Country ) )]
+    public int CountryId { get; set; }
+
+    #endregion
+
+    #region Navigation Properties
+
+    /// <summary>
+    /// The Country related to this city.
+    /// </summary>
+
+    public CountryModel? Country { get; set; } = null;
+
+    #endregion
+
+
+}
---
+++ WorldCitiesApi/Data/Models/CountryModel.cs
@@ -0,0 +1,49 @@
+namespace WorldCitiesApi.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class CountryModel
+{
+
+    #region Properties
+
+    ///<summary>
+    /// The unique id and primary key for this country
+    /// </summary
+
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    ///<summary>
+    /// Country Name (in UTF8 format)
+    /// </summary>
+
+    public string Name { get; set; } = null;
+
+    ///<summary>
+    /// Country Code (in ISO 3166-1 ALPHA-2 format
+    /// </summary
+
+    public string ISO2 { get; set; } = null;
+
+    ///<summary>
+    /// Country Code (in ISO 2166-1 ALPHA-3 format)
+    /// </summary>
+
+    public string ISO3 { get; set; } = null;
+
+    #endregion
+
+    #region Navigation Properties
+
+    ///<summary>
+    ///A collection of all cities related to this 
+    ///country
+    ///</summary>
+
+    public ICollection<CityModel>? Cities { get; set; } = null;
+
+    #endregion
+
+}
